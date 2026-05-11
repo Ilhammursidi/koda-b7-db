5866dc0cdfdcb0a2b416b7d33f1efd90ca8712de
ilhammursidi <imursidi37@gmail.com>
2026-05-11 17:21:09 +0700

    feat: re-create table

--- ewallet.sql
+++ ewallet.sql
@@ -7,9 +7,9 @@ CREATE TABLE users (
     photo_path VARCHAR(255),
     phone_number VARCHAR(255) UNIQUE,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ,
     deleted_at TIMESTAMPTZ
-)
+);
 
 CREATE TABLE wallet (
     id SERIAL PRIMARY KEY,
@@ -21,17 +21,23 @@ CREATE TABLE wallet (
 
 CREATE TYPE transaction_status AS ENUM ('PENDING','SUCCESS','FAILED');
 
-CREATE TABLE transaction (
+CREATE TABLE transactions (
     id SERIAL PRIMARY KEY,
-    wallet_id INT NOT NULL,
+    user_id INT NOT NULL,
+    sender_wallet_id INT,
+    receiver_wallet_id INT,
+    payment_method_id INT,
     type VARCHAR(255) NOT NULL,
     amount INT NOT NULL,
-    status transaction_status NOT NULL DEFAULT 'PENDING',
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    FOREIGN KEY (wallet_id) REFERENCES wallet(id)
+    status transaction_status DEFAULT 'PENDING',
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ,
+    FOREIGN KEY (sender_wallet_id) REFERENCES wallet(id),
+    FOREIGN KEY (receiver_wallet_id) REFERENCES wallet(id),
+    FOREIGN KEY (payment_method_id) REFERENCES payment_methods(id)
 )
 
-CREATE TABLE transfer (
+CREATE TABLE transfer_details (
     id SERIAL PRIMARY KEY,
     sender_wallet_id INT NOT NULL,
     receiver_wallet_id INT NOT NULL,
@@ -46,12 +52,33 @@ CREATE TABLE payment_methods (
     payment_name VARCHAR(255) NOT NULL
 )
 
-CREATE TABLE topup (
+CREATE TABLE topup_details (
     id SERIAL PRIMARY KEY,
     wallet_id INT NOT NULL,
-    amount INT NOT NULL,
     payment_method_id INT NOT NULL,
+    order_amount INT NOT NULL,
+    tax_amount INT NOT NULL,
+    delivery_fee INT NOT NULL,
+    total_amount INT NOT NULL,
+    status transaction_status NOT NULL DEFAULT 'PENDING',
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     FOREIGN KEY (wallet_id) REFERENCES wallet(id),
     FOREIGN KEY (payment_method_id) REFERENCES payment_methods(id)
 )
+
+-- ROLLBACK
+
+-- BEGIN;
+-- TRUNCATE TABLE transactions RESTART IDENTITY CASCADE;
+-- DROP TABLE transactions CASCADE;
+-- TRUNCATE TABLE users RESTART IDENTITY CASCADE;
+-- DROP TABLE users CASCADE;
+-- TRUNCATE TABLE topup_details RESTART IDENTITY CASCADE;
+-- DROP TABLE topup_details CASCADE;
+-- TRUNCATE TABLE transfer_details RESTART IDENTITY CASCADE;
+-- DROP TABLE transfer_details CASCADE;
+-- TRUNCATE TABLE wallet RESTART IDENTITY CASCADE;
+-- DROP TABLE wallet CASCADE;
+-- TRUNCATE TABLE payment_methods RESTART IDENTITY CASCADE;
+-- DROP TABLE payment_methods CASCADE;
+-- COMMIT;
